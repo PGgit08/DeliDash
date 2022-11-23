a2e10156f84a9612db1e4ba3d66098319df5d72f
PGgit08 <60079012+PGgit08@users.noreply.github.com>
2022-11-22 19:27:54 -0500

    created a new react-native project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const App = () => {
+	return (
+		<View style={styles.container}>
+			<Text>Hello World!</Text>
+			<Button
+				onPress = {() => alert('What\'s Up! Button Pressed.')}
+				title = "Press Me!"
+			/>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
+
+export default App;
